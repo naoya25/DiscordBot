7121c3689cb32c33dc5030e5cd4caf41cfdc3f0f
naoya25 <onaoya2002@gmail.com>
2023-09-15 13:29:33 +0900

    hello返信bot完成

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'送信: {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+
+        if message.content == '$Hello':
+            await message.channel.send('Hello!')
 
 intents = discord.Intents.default()
 intents.message_content = True
