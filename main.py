9f2c7c94b217df04af8580df1429601b2e325e76
naoya25 <onaoya2002@gmail.com>
2023-09-17 22:20:26 +0900

    download

--- main.py
+++ main.py
@@ -58,17 +58,25 @@ tree = discord.app_commands.CommandTree(client)
 async def positive(interaction: discord.Interaction, user: discord.Member):
     await interaction.response.defer()
     userposts = getRecentData(interaction.guild.id, userid=user.id)
-    posiscore = sum([i[5] for i in userposts]) / len(userposts)
-    text = f'{user.display_name}のpositive度合いは: {round(posiscore*100, 1)}%です！'
-    await interaction.followup.send(text, ephemeral=False)
+    if len(userposts) > 0:
+        posiscore = sum([i[5] for i in userposts]) / len(userposts)
+        text = f'{user.display_name}のpositive度合いは: {round(posiscore*100, 1)}%です！'
+        await interaction.followup.send(text, ephemeral=False)
+    else:
+        text = f'{user.display_name}の投稿が見つかりません'
+        await interaction.followup.send(text, ephemeral=False)
 
 @tree.command(name='negative',description='userのnegative数値を返します！')
 async def negative(interaction: discord.Interaction, user: discord.Member):
     await interaction.response.defer()
     userposts = getRecentData(interaction.guild.id, userid=user.id)
-    negascore = sum([i[7] for i in userposts]) / len(userposts)
-    text = f'{user.display_name}のnegative度合いは: {round(negascore*100, 1)}%です！'
-    await interaction.followup.send(text, ephemeral=False)
+    if len(userposts)> 0:
+        negascore = sum([i[7] for i in userposts]) / len(userposts)
+        text = f'{user.display_name}のnegative度合いは: {round(negascore*100, 1)}%です！'
+        await interaction.followup.send(text, ephemeral=False)
+    else:
+        text = f'{user.display_name}の投稿が見つかりません'
+        await interaction.followup.send(text, ephemeral=False)
 
 @tree.command(name='download_sentiment',description='このサーバー全てのネガポジをダウンロードします！')
 async def download_csv(interaction: discord.Interaction):
