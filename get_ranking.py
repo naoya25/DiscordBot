2d9e835e53d13f6d53cd0cffe95a556f3cdd9cdd
naoya25 <onaoya2002@gmail.com>
2023-09-17 17:54:45 +0900

    スラッシュコマンドnegaposi userごと

--- get_ranking.py
+++ get_ranking.py
@@ -2,14 +2,24 @@ import sqlite3
 from datetime import datetime, timedelta
 import numpy as np
 
-def getRecentData(guildid, pastDays):
+def getRecentData(guildid, pastDays=-1, userid=0):
     start_date = datetime.utcnow() - timedelta(days=pastDays)
 
     with sqlite3.connect('discord_db') as connection:
         cursor = connection.cursor()
 
-        query = "SELECT * FROM Post WHERE guildid = ? AND created_at >= ?"
-        cursor.execute(query, (guildid, start_date))
+        if pastDays == -1 and userid == 0:
+            query = "SELECT * FROM Post WHERE guildid = ?"
+            cursor.execute(query, (guildid, ))
+        elif pastDays > -1 and userid == 0:
+            query = "SELECT * FROM Post WHERE guildid = ? AND created_at >= ?"
+            cursor.execute(query, (guildid, start_date))
+        elif pastDays == -1 and userid != 0:
+            query = "SELECT * FROM Post WHERE guildid = ? AND userid = ?"
+            cursor.execute(query, (guildid, userid))
+        else:
+            query = "SELECT * FROM Post WHERE guildid = ? AND created_at >= ? AND userid = ?"
+            cursor.execute(query, (guildid, start_date, userid))
         posts = cursor.fetchall()
         return posts
 
--- main.py
+++ main.py
@@ -18,24 +18,25 @@ class MyClient(discord.Client):
                 posts = getRecentData(int(os.getenv('GUILDID')), 7)
                 positive_ranking, negative_ranking = calculateUserSentiment(posts)
                 text = '今週のモチベランキング\npositive\n'
-                text += '\n'.join([f'> {i}. {self.get_user(p[0])}' for i, p in enumerate(positive_ranking)])
+                text += '\n'.join([f'> {i}. {await self.fetch_user(p[0])}' for i, p in enumerate(positive_ranking)])
                 text += '\nnegative\n'
-                text += '\n'.join([f'> {i}. {self.get_user(n[0])}' for i, n in enumerate(negative_ranking)])
+                text += '\n'.join([f'> {i}. {await self.fetch_user(n[0])}' for i, n in enumerate(negative_ranking)])
                 await channel.send(text)
             if now.day == 1 and now.hour == 12:
                 channel = self.get_channel(int(os.getenv('CHANNELID')))
                 posts = getRecentData(int(os.getenv('GUILDID')), 30)
                 positive_ranking, negative_ranking = calculateUserSentiment(posts)
                 text = '今月のモチベランキング\npositive\n'
-                text += '\n'.join([f'> {i}. {self.get_user(p[0])}' for i, p in enumerate(positive_ranking)])
+                text += '\n'.join([f'> {i}. {await self.fetch_user(p[0])}' for i, p in enumerate(positive_ranking)])
                 text += '\nnegative\n'
-                text += '\n'.join([f'> {i}. {self.get_user(n[0])}' for i, n in enumerate(negative_ranking)])
+                text += '\n'.join([f'> {i}. {await self.fetch_user(n[0])}' for i, n in enumerate(negative_ranking)])
                 await channel.send(text)
             await asyncio.sleep(60*60)
 
     async def on_ready(self):
         print(f'ログインしました: {self.user}')
         self.loop.create_task(self.send_ranking())
+        await tree.sync()
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -51,6 +52,22 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+@tree.command(name='positive',description='userのpositive数値を返します！')
+async def test_command(interaction: discord.Interaction, user: discord.Member):
+    userposts = getRecentData(interaction.guild.id, userid=user.id)
+    posiscore = sum([i[5] for i in userposts]) / len(userposts)
+    text = f'{user.display_name}のpositive度合いは: {round(posiscore*100, 1)}%です！'
+    await interaction.response.send_message(text, ephemeral=False)
+
+@tree.command(name='negative',description='userのnegative数値を返します！')
+async def test_command(interaction: discord.Interaction, user: discord.Member):
+    userposts = getRecentData(interaction.guild.id, userid=user.id)
+    negascore = sum([i[7] for i in userposts]) / len(userposts)
+    text = f'{user.display_name}のnegative度合いは: {round(negascore*100, 1)}%です！'
+    await interaction.response.send_message(text, ephemeral=False)
+
 
 # keep_alive()
 # try:
